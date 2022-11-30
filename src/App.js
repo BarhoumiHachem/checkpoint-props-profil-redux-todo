c431d5a5e8d2fb80f6a267efca1f19786364ee13
BarhoumiHachem <barhoumi.hachem.10@gmail.com>
2022-11-30 13:48:42 +0100

    checkpoint react-props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import Profile from './profile/Profile';
+import img from './profile/photo.jpg';
 import './App.css';
 
 function App() {
+  const fullName="Hachem Barhoumi";
+  const bio= "holder of a degree in computer science and a degree in fullstack js, Passionate about web development. Self-taught and curious.";
+  const profession="Web Developer";
+  const stylePhoto={height: "400px", borderRadius: "5%" , marginLeft: "40px"};
+  const styleName={color: "#d81159", fontSize: "40px", padding: "20px 0", fontFamily: "Lucida Handwriting"};
+  const styleProf={color: "#50514f", fontSize: "40px", paddingBottom: "20px", fontStyle: "italic", fontFamily: "Brush Script MT"};
+  const styleBio={color: "#1b4965", fontSize: "20px", paddingBottom: "100px", fontFamily: "Georgia"};
+  const handleName=()=>{
+    alert(fullName)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile fullName={fullName}
+                profession={profession} 
+                bio={bio}
+                stylePhoto={stylePhoto} 
+                styleName={styleName}
+                styleProf={styleProf}
+                styleBio= {styleBio}
+                handleName={handleName}>
+                {img}
+      </Profile>
     </div>
   );
 }
